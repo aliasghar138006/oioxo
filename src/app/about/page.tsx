7fd2896642d3bf01e46bfe7e862d2266c92e5b12
aliasghar138006 <aliasgharshahneh1380@gmail.com>
2025-07-03 23:56:34 +0330

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,14 @@
+import Image from "next/image"
 import styles from "./page.module.css"
+import back from "@images/back.jpg"
 
 function page() {
   return (
-   <div className="bg-gradient-to-r from-[blue] to-[red] z-10">
-     <div className={styles.container}>
+   <div className="relative bg-gradient-to-r from-[#330DDC] via-[#B547FF] to-[#F38831] z-10">
+    <h1 className="absolute left-[15%] top-[20%] text-center text-white font-bold text-[48px]">Publish Your site with lightning Speed</h1>
+    <button style={{padding:"10px 32px" , backgroundColor:"rgba(255, 255, 255, 0.2)"}} className="absolute cursor-pointer left-[45%] top-[50%] border border-[#FFFFFF] text-[white] rounded-[100px]">Get Started</button>
         
-    </div>
+     <Image className="w-[1440px] h-[265px] opacity-20" width={1000} height={1000} src={back} alt="back" />
    </div>
   )
 }
