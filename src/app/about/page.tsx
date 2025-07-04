25e219a0fb9ea648576549240ccc9ce46f7a6f6b
aliasghar138006 <aliasgharshahneh1380@gmail.com>
2025-07-04 23:29:52 +0330

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,11 +4,14 @@ import back from "@images/back.jpg"
 
 function page() {
   return (
-   <div className="relative bg-gradient-to-r from-[#330DDC] via-[#B547FF] to-[#F38831] z-10">
-    <h1 className="absolute left-[15%] top-[20%] text-center text-white font-bold text-[48px]">Publish Your site with lightning Speed</h1>
-    <button style={{padding:"10px 32px" , backgroundColor:"rgba(255, 255, 255, 0.2)"}} className="absolute cursor-pointer left-[45%] top-[50%] border border-[#FFFFFF] text-[white] rounded-[100px]">Get Started</button>
+   <div className="w-[100%] h-[265px] overflow-hidden relative">
+    <div className="w-[100%] h-[265px] overflow-hidden absolute bg-gradient-to-r from-[10%] from-[#7B5EFF] via-35% via-[#B547FF] to-80% to-[#F38831]"></div>
+    <h1 className="absolute left-[15%] top-[20%] text-center text-white font-[700] text-[48px] z-20">Publish Your site with lightning Speed</h1>
+    <button style={{padding:"10px 32px" , backgroundColor:"rgba(255, 255, 255, 0.2)"}} className="absolute cursor-pointer left-[45%] top-[55%] border border-[#FFFFFF] text-[white] rounded-[100px] z-20">Get Started</button>
+     <Image style={{marginBottom:"100px"}} className="absolute top-[-200px] overflow-hidden opacity-30" width={1440} height={265} src={back} alt="back" />
+    {/* <div className="absolute w-[100%] h-[265px] bg-[#00000066] opacity-[40%]"></div> */}
         
-     <Image className="w-[1440px] h-[265px] opacity-20" width={1000} height={1000} src={back} alt="back" />
+   
    </div>
   )
 }
